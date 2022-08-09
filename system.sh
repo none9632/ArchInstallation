a27110264425c0afbc79409e99cfc838dd44e7fa
none9632 <none9632@protonmail.com>
2022-08-10 02:19:44 +0300

    Minor changes

--- system.sh
+++ system.sh
@@ -93,9 +93,10 @@ then
     sudo pkgfile --update
 fi
 
-# vim-plug
+# neovim
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+pip install pynvim
 
 # git configuration
 git config --global user.email "none9632@protonmail.com"
